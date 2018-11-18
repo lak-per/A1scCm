8bbd6e1b36d37737e3126c6762ec517a51def0c7
hswaroopkato <hswaroopkato@DA16010730.global.avaya.com>
2018-11-18 16:02:56 +0530

    Small

--- src/test/java/testBase/GroundFloor.java
+++ src/test/java/testBase/GroundFloor.java
@@ -60,23 +60,22 @@ public class GroundFloor {
 			objectRepoFile.load(fis);
 			log.info("Loading Object Repository");
 
-			if (System.getProperty("browser") != null
-					&& System.getProperty("browser").isEmpty()) {
-				browser = System.getProperty("browser");
+			if (System.getenv("browser") != null
+					&& !System.getenv("browser").isEmpty()) {
+				browser = System.getenv("browser");
 			} else {
 				browser = configFile.getProperty("browser").toLowerCase();
 			}
-			// configFile.setProperty("browser", browser);
+			configFile.setProperty("browser", browser);
 
-			if (System.getProperty("QRN") != null
-					&& System.getProperty("QRN").isEmpty()) {
-				QRN = System.getProperty("QRN");
+			if (System.getenv("QRN") != null && !System.getenv("QRN").isEmpty()) {
+				QRN = System.getenv("QRN");
 			} else {
 				QRN = objectRepoFile.getProperty("quoteNumber");
 			}
 			objectRepoFile.setProperty("quoteNumber", QRN);
 
-			switch (browser) {
+			switch (configFile.getProperty("browser").toString()) {
 
 			case "chrome":
 				System.setProperty(
